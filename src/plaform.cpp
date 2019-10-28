87d481753f46a4c23867507571f990bb2f5e9d83
Lorna <eleonora.dups@gmail.com>
2019-10-28 20:17:29 +0100

    corrected bug moving platform

--- src/plaform.cpp
+++ src/plaform.cpp
@@ -39,5 +39,18 @@ MovingPlatform::MovingPlatform(sf::Vector2f position, sf::Vector2f size) : Platf
 
 void MovingPlatform::Moving()
 {
-	platformBody_->SetLinearVelocity(b2Vec2(-0.5, 0));
+	// platformBody_->SetLinearVelocity(b2Vec2(0.5, 0));
+	// platformBody_->SetLinearVelocity(b2Vec2(-0.5, 0));
+
+	b2Vec2 position = platformBody_->GetPosition();
+
+	if (position.x > 5)
+	{
+		platformBody_->SetLinearVelocity(b2Vec2(-0.5, 0));
+	}
+	if (position.x < 3)
+	{
+		platformBody_->SetLinearVelocity(b2Vec2(0.5, 0));
+	}
+
 }
